d51b1e78b9895f22b80de64a6780363631b0af7d
Helena Stage <helena.stage@manchester.ac.uk>
2020-11-30 15:14:16 +0000

    Add files via upload

--- reopening/InstGrowthRates.r
+++ reopening/InstGrowthRates.r
@@ -10,8 +10,8 @@ DataFile <- function(country){
   filename
 }
 
-GrowthRate <- function(raw, country, plotfig=TRUE, printrates=TRUE){
-  
+GrowthRate <- function(raw, country, FE='None', meth="ML", splinemeth='bs', plotfig=TRUE, printrates=TRUE){
+  startdate <- "2020-04-01"#raw$Dates[1]
   if(country=="DK"){raw <- raw$Hospital}
   else if(country=="DKcase"){raw <- raw$Confirmed}
   else if(country=="NO"){raw <- raw$Confirmed}
@@ -33,9 +33,43 @@ GrowthRate <- function(raw, country, plotfig=TRUE, printrates=TRUE){
   newdayseps <- data.frame(ndays=dteps)
   
   #Define GAM function
-  GAMfit <- gam(raw~s(ndays), family=quasipoisson())
-  #Estimated fits for times (t, t+epsilon)
+  GAMfit <- gam(raw~s(ndays), family=nb())
+  #Define GAM function with fixed effect for weekends
+  if(FE=='None'){
+    GAMfit <- gam(raw~s(ndays, bs=splinemeth), family=nb(), method=meth)
+  }else{
+    DW <- weekdays(as.Date(ndays, origin = startdate))
+    if(FE=='WE'){
+      DW <- ifelse(DW=='Sunday','WE',ifelse(DW=='Saturday','WE','WD'))
+    }
+    GAMfit <- gam(raw~s(ndays, bs=splinemeth) + DW, family=nb(), method=meth)
+  }
+  #gam.check(GAMfit)
+  
+  #Define data structure with day-of-week effect
+  if(FE=='None'){
+    newdays <- data.frame(ndays=dt)
+  }else{
+    dow <- weekdays(as.Date(dt, origin = startdate))
+    if(FE=='WE'){
+      dow <- ifelse(dow=='Sunday','WE',ifelse(dow=='Saturday','WE','WD'))
+    }
+    newdays <- data.frame(ndays=dt, DW=dow)
+  }
+  #Estimated fits for times t
   Fitt <- predict(GAMfit, newdays, type="lpmatrix")
+  
+  #Define data structure with day-of-week effect at time t + epsilon
+  if(FE=='None'){
+    newdayseps <- data.frame(ndays=dteps)
+  }else{
+    dow <- weekdays(as.Date(dteps, origin = startdate))
+    if(FE=='WE'){
+      dow <- ifelse(dow=='Sunday','WE',ifelse(dow=='Saturday','WE','WD'))
+    }
+    newdayseps <- data.frame(ndays=dteps, DW=dow)
+  }
+  #Estimated fits for times (t, t+epsilon)
   Fitteps <- predict(GAMfit, newdayseps, type="lpmatrix")
   
   #Approximation of the derivative of the fit. This derivative will give us the instantaneous rate.
@@ -50,6 +84,22 @@ GrowthRate <- function(raw, country, plotfig=TRUE, printrates=TRUE){
   rates$rate <- rateval
   rates$rateub <- rateval+2*rateval.stdev
   rates$ratelb <- rateval-2*rateval.stdev
+  
+  par(mfrow=c(1,2))
+  plot(as.Date(ndays, origin = startdate), raw, ylab='Cases', xlab='', ylim=range(c(1.5*max(raw),0)))
+  
+  p <- predict(GAMfit, type = "link", se.fit = TRUE)
+  upr <- p$fit + (2 * p$se.fit)
+  lwr <- p$fit - (2 * p$se.fit)
+  upr <- GAMfit$family$linkinv(upr)
+  lwr <- GAMfit$family$linkinv(lwr)
+  lines(as.Date(ndays, origin = startdate), GAMfit$family$linkinv(p$fit), col=1)
+  lines(as.Date(ndays, origin = startdate), upr, col=1, lty=2)
+  lines(as.Date(ndays, origin = startdate), lwr, col=1, lty=2)
+  
+  plot(as.Date(dteps, origin = startdate),rateval,type="l",ylim=range(c(rateval+2*rateval.stdev,rateval-2*rateval.stdev)), ylab='Instantaneous growth rate', xlab='')
+  lines(as.Date(dteps, origin = startdate),rateval+2*rateval.stdev,lty=2)
+  lines(as.Date(dteps, origin = startdate),rateval-2*rateval.stdev,lty=2)
   
   if(printrates==TRUE){
     if(country=="DK"){
@@ -86,11 +136,11 @@ GrowthRate <- function(raw, country, plotfig=TRUE, printrates=TRUE){
 }
 
 #Choose which country to analyse (DK, DKcase, NO, DE, DEstaff, or DEstudents):
-CountryCode <- "NO"
+CountryCode <- "DEstudents"
 InputData<-data.frame(read.csv(DataFile(CountryCode)))
 #Toggle printrates if you want to save the instantaneous growth rates
 #Evaluates the instantaneous growth rate
-GrowthRate(InputData, CountryCode, printrates=FALSE)
+GrowthRate(InputData, CountryCode, printrates=TRUE, FE="WD", splinemeth = 'tp')
 #These growth rates are visualised in Python (see other enclosed scripts)
 
 
